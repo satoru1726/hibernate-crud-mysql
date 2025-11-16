83d4f23bfd569aaf2a43e7c088d53fde63f91e6d
Jayanth A R <127710029+satoru1726@users.noreply.github.com>
2025-11-17 00:16:38 +0530

    updated certain files.

--- src/main/java/in/elmarino/Main.java
+++ src/main/java/in/elmarino/Main.java
@@ -5,65 +5,52 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-/* ORM - Object Relational Mapping/Mapper -- it'll store an object in database without having to write rdbms query manually.
-   ORM tool - best example - Hibernate[everything data!] */
 public class Main {
-    static void main(String[] args) {
+static void main(String[] args) {
 
-        StudentName name = new StudentName();
-        name.setFname("Jayanth");
-        name.setMname("Ramachandra");
-        name.setLname("Anekere");
+StudentName name = new StudentName();
+name.setFname("first-name");
+name.setMname("middle-name");
+name.setLname("last-name");
 
-        StudentClassForFetch student01 = new StudentClassForFetch();
+StudentClassForFetch student01 = new StudentClassForFetch();
 
-        student01.setRollNum(85);
-        student01.setName(String.valueOf(name)); // embeddable concept -- name is an instance of StudentName class that has first, middle and last names for a student name.
-        student01.setFavLang("Java + SpringBoot");
+student01.setRollNum(001);
+student01.setName(String.valueOf(name)); 
+student01.setFavLang("Java + SpringBoot");
 
-        // Configuration config = new Configuration();
-        // config.addAnnotatedClass(in.elmarino.StudentClassForFetch.class); // ** here you need to mention the fully qualified class name.
-        // config.configure("cfg.xml"); // pass the configuration file here. [by default it'll read hibernate.cfg.xml]
+SessionFactory factory = new Configuration().addAnnotatedClass(StudentClassForFetch.class).configure("cfg.xml").buildSessionFactory();
 
-        // SessionFactory factory = config.builSessionFactory();
-        // ** you can write the above four statements in a single line
+Session session = factory.openSession();
 
-        SessionFactory factory = new Configuration().addAnnotatedClass(StudentClassForFetch.class).configure("cfg.xml").buildSessionFactory();
+Transaction transaction = session.beginTransaction();
+session.persist(student01); 
 
-        Session session = factory.openSession();
+// to update data
+session.merge(student01);
 
-        Transaction transaction = session.beginTransaction();
+// to delete data
+session.remove(student01);
+   
+transaction.commit();
 
-           // session.save(student01); // save() is deprecated since JAP(Java/Jakarta Persistence API), use persist() method.
-session.persist(student01); // pass your object as an argument here --> it'll fire an inset query.
+session.close();
+factory.close();
 
-        // to update data ***
-/* session.merge(student01); // --> if the object exists then it'll update it, if it doesn't then it'll create it.
+// code for fetch operation [note: run it on a seperate file.]
+org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+configuration.addAnnotatedClass(StudentClassForORM.class);
+configuration.configure("configurationFile.xml");
 
-        /* to delete data ***
-session.remove(student01); */
+SessionFactory sessionFactory = configuration.buildSessionFactory();
+Session session = sessionFactory.openSession();
 
-        transaction.commit(); // ** this statement is most impt.,
+StudentClassForORM student = session.byId(StudentClassForORM.class).getReference(3);
 
-        session.close(); // --> it's important to close the session and factory objects.
-        factory.close();
+System.out.println(student);
 
-        // code for fetch operation
-        /* org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
-        configuration.addAnnotatedClass(StudentClassForORM.class);
-        configuration.configure("configurationFile.xml");
+session.close();
+sessionFactory.close(); 
 
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.openSession();
-
-        // StudentClassForORM student = session.get(StudentClassForORM.class, 3); // this is deprecated, use find() -- from JPA --> get() or find() is an example for "Eager fetching" ****
-
-        // StudentClassForORM studentClassForORM = session.find(StudentClassForORM.class, 6); or you can also use StudentClassForORM student = session.byId(StudentClassForORM.class).load(6) or the below one too
-        StudentClassForORM student = session.byId(StudentClassForORM.class).getReference(3); // this is "Lazy fetching" *****
-
-        System.out.println(student);
-
-        session.close();
-        sessionFactory.close(); */
-    }
+}
 }
